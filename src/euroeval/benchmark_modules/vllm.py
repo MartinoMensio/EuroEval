0b246a5ddb9023cb8e36dd280f2c184a51ddaeaa
Martino Mensio <martino.mensio@tno.nl>
2025-12-09 16:03:58 +0100

    debug

--- src/euroeval/benchmark_modules/vllm.py
+++ src/euroeval/benchmark_modules/vllm.py
@@ -507,6 +507,7 @@ class VLLMModel(HuggingFaceEncoderModel):
         # sampling_params.temperature=0.0
         sampling_params.stop=[stop_token for stop_token in stop_tokens if stop_token]
         sampling_params.structured_outputs=structured_outputs
+        log(f"sampling_params.temperature={sampling_params.temperature}")
 
         # If any of the prompts are empty then we need to replace them with a BOS token
         # so that the vLLM model can generate from them
